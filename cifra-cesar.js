9c7acad0777792b60f5f7c0393b1690d8ac21df1
Vitoria Felix <vf23708@gmail.com>
2022-11-19 17:27:04 -0300

    subindo atividade cifra-cesar

---
+++ cifra-cesar.js
@@ -0,0 +1,19 @@
+var entrada = require('readline-sync')
+
+let palavraUsuario = entrada.question('Informe uma palavra sem numeros e caracteres especiais: ');
+
+console.log('Sua palavra criptografada é: ' + criptografarPalavra(palavraUsuario));
+
+function criptografarPalavra(palavra) {
+    const alfabeto = ['a', 'b', 'c', 'd', 'e', 'f','g', 'h', 'i', 
+    'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r','s', 't',
+     'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5'];
+    let palavraCriptografada = '';
+
+     for (let index = 0; index < palavra.length; index++) {
+        const letra = palavra[index];
+        const letraCriptografada = alfabeto[alfabeto.indexOf(letra) + 5];
+        palavraCriptografada += letraCriptografada;
+    }
+    return palavraCriptografada;
+}
